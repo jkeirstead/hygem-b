fec27aa06e3dbc954029a1260fbb6064db08cfed
James Keirstead <james.keirstead@gmail.com>
2013-06-25 21:08:12 +0900

    Forgot to change the default value

--- hygem-b.r
+++ hygem-b.r
@@ -10,7 +10,7 @@
 ## Runs the HyGEM model of energy demand in the global buildings sector.  The model consists of two components: a top-down regression model which predicts energy consumption in ten world regions by fuel type; and a bottom-up technical model which calculates the possible energy and emission savings from five key interventions: improved space heating efficiency, increased use of ground-source heat pumps, improved efficiency of electrical appliances and lighting, fuel transfer (from fossil fuels to other low carbon vectors), and decarbonization of the electricity grid.
 ##
 ## @param run_top_down a Boolean indicating whether the top-down model should be run.  By default = TRUE, but this requires users to manually download a data set from the IEA.  By setting the option to a FALSE a set of precompiled results will be used.
-run_hygem <- function(run_top_down=FALSE) {
+run_hygem <- function(run_top_down=TRUE) {
 
   ## Load required library
   library(knitr)
