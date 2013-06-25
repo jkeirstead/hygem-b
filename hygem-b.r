ed9d0ddc2ea8a84b985fa21deb8c5a0c6e85f8ff
James Keirstead <james.keirstead@gmail.com>
2013-06-25 21:05:16 +0900

    Tidied up main model files

--- hygem-b.r
+++ hygem-b.r
@@ -7,17 +7,17 @@
 
 ## Run the HyGEM model
 ##
-## Runs the HyGEM model of energy demand in the global buildings sector.  The model consists of two components: a top-down regression model which predicts energy consumption in ten world regions by fuel type; and a bottom-up technical model which calculates the likely mix of technologies that meet this target.  
+## Runs the HyGEM model of energy demand in the global buildings sector.  The model consists of two components: a top-down regression model which predicts energy consumption in ten world regions by fuel type; and a bottom-up technical model which calculates the possible energy and emission savings from five key interventions: improved space heating efficiency, increased use of ground-source heat pumps, improved efficiency of electrical appliances and lighting, fuel transfer (from fossil fuels to other low carbon vectors), and decarbonization of the electricity grid.
 ##
-## @param use_TD_archive a Boolean indicating whether pre-compiled top-down model results should be used, default = FALSE.  Users must manually download a data set from the IEA to use the top-down model and so this option allows users to skip directly to the bottom-up component.
-run_hygem <- function(use_TD_archive=FALSE) {
+## @param run_top_down a Boolean indicating whether the top-down model should be run.  By default = TRUE, but this requires users to manually download a data set from the IEA.  By setting the option to a FALSE a set of precompiled results will be used.
+run_hygem <- function(run_top_down=FALSE) {
 
   ## Load required library
   library(knitr)
 
   ## Save the archive flag
   e1 <- new.env(parent=environment())
-  assign("use_TD_archive", use_TD_archive, envir=e1)
+  assign("run_top_down", run_top_down, envir=e1)
   
   ## Run the model
   knit2html("code/hygem-b.Rmd", envir=e1)
