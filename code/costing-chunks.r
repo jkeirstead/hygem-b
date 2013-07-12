518804315c89acbdf7343b2aef38094ca7e003d7
James Keirstead <james.keirstead@gmail.com>
2013-07-12 15:25:04 +0100

    Added the electrical efficiency costs

--- code/costing-chunks.r
+++ code/costing-chunks.r
@@ -23,10 +23,10 @@ tmp <- transform(tmp, scenario=ifelse(scenario=="lowC", "LCS", "LMS"))
 ## Assume 1.671 USD to GBP, British pound sterling to US dollar exchange rate, average 1999 - 2012
 tmp <- mutate(tmp, number=households*penetration, cost=number*size*990*1.671)
 tmp.mt <- dcast(tmp, year + region ~ scenario, value.var="cost")
-tmp.mt <- transform(tmp.mt, cost=LCS-LMS)
-heat_pumps <- tmp.mt[,c("region", "cost")]
+tmp.mt <- transform(tmp.mt, cost=LCS-LMS, discount=0.035, years=25)
+gshp_costs <- tmp.mt[,c("region", "cost", "discount", "years")]
 
-tmp <- transform(heat_pumps, cost=cost/1e9)
+tmp <- transform(gshp_costs, cost=cost/1e9)
 names(tmp) <- c("Region", "Capital cost")
 tmp.xt <- xtable(tmp[,1:2],
                  digits=c(0,0,2),
@@ -38,7 +38,23 @@ print(tmp.xt, include.rownames=FALSE,  type="html",
 
 
 ## @knitr efficiency-costs
-message('Not yet implemented')
+message('Note these results are calculated in the supplementary spreadsheet efficiency_costs.xlsx and hard-coded here for convenience.  Future versions will port this to R')
+eff_costs <- data.frame(region=space_costs$region,
+                        cost=c(25441583799.8172, 13659324918.2762, 18678417577.204,
+                          9416235099.30109, 10713407116.5336, 22166510116.8187,
+                          10117046204.5931, 80653591310.8488, 117181800698.091, 11780127109.4934),
+                        discount=0.035, years=15)
+tmp <- transform(eff_costs, cost=cost/1e9)[,1:2]
+names(tmp) <- c("Region", "Capital cost")
+
+tmp.xt <- xtable(tmp,
+                 digits=c(0,0,2),
+                 align="llr",
+                 caption="Capital cost of lighting and appliance efficiency investments (billion USD)")
+print(tmp.xt, include.rownames=FALSE,  type="html",
+      html.table.attributes=getOption("xtable.html.table.attributes",
+                          "border=1 width=400"))
+
 
 ## @knitr other-costs
 other_costs <- data.frame(space_costs$region, fuel=0, grid=0, discount=0, years=0)
